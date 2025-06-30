b0de3c7e5c74aaddcd045ac94900eb02f8b61b9e
theMungai <mungaithesimon@gmail.com>
2025-06-30 08:15:18 +0300

    remove the circular import

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
-from app import db, bc
+from app import db, bcrypt, jwt
+
 from app.models import User
 from flask_jwt_extended import create_access_token
 from sqlalchemy.exc import IntegrityError
@@ -15,7 +16,7 @@ def register():
     if not username or not password:
         return jsonify({"msg": "Username and password required"}), 400
 
-    pw_hash = bc.generate_password_hash(password).decode("utf-8")
+    pw_hash = bcrypt.generate_password_hash(password).decode("utf-8")
     user = User(username=username, password_hash=pw_hash)
 
     try:
@@ -33,7 +34,7 @@ def login():
     password = data.get("password")
 
     user = User.query.filter_by(username=username).first()
-    if user and bc.check_password_hash(user.password_hash, password):
+    if user and bcrypt.check_password_hash(user.password_hash, password):
         access_token = create_access_token(identity=str(user.id))
         return jsonify(access_token=access_token, username=user.username, role=user.role)
     return jsonify({"msg": "Bad username or password"}), 401
